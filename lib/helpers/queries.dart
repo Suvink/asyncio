7951a0a1890804ec7c18496a2d5fb8f3486046be
Isala Piyarisi <code@isala.me>
2019-06-01 15:01:14 +0530

    8==========D

--- lib/helpers/queries.dart
+++ lib/helpers/queries.dart
@@ -1,3 +1,70 @@
 class Queries {
-  String news = "{" + "}";
+  String news = r"""query {
+      news{
+        id
+        title
+        time
+        link
+        source{
+          id
+          name
+          logo
+        }
+        image
+      }
+    }
+  """;
+
+  String events = r"""query {
+      events{
+        id
+        name
+        venue
+        organizer
+        date
+        time
+        description
+        registrationLink
+        banner
+      }
+    }
+  """;
+
+  String tutorials = r"""query {
+      tutorials{
+        id
+        title
+        author
+        publishedDate
+        link
+        source{
+          id
+          name
+          logo
+        }
+      }
+    }
+  """;
+
+  String communities = r"""query {
+      communities{
+        id
+        name
+        description
+        ircLink
+        communityLink
+      }
+    }
+  """;
+
+  String widgets = r"""query {
+        widgets{
+          id
+          name
+          description
+          image
+          link
+        }
+    }
+  """;
 }
